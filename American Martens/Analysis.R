34e3279b91eb931c62558da6913ab31196e9099d
clarapasu <62282526+clarapasu@users.noreply.github.com>
2025-07-17 12:53:58 +0200

    Update Analysis.R

--- American Martens/Analysis.R
+++ American Martens/Analysis.R
@@ -17,8 +17,8 @@ source("Functions/Fit_Func.R")
 ## - in data we have the capture histories consisting of three variables 
 ##   representing an individual's unique ID, the time of the capture
 ##   and the ID of the camera trap where the capture occurred.
-data<-read.csv("marten_data.csv")
-traps<-read.csv("marten_traps.csv")
+data<-read.csv("American Martens/marten_data.csv")
+traps<-read.csv("American Martens/marten_traps.csv")
 
 
 ## create the mask over the region; different buffers were tested 
@@ -32,18 +32,31 @@ traps<-as.matrix(traps)
 
 
 ## to produce Figure 1 of the paper run the following code
-# mask_plot = make.mask(trap, buffer = 2, spacing = 0.01, type = "trapbuffer")
-# plot(mask_plot, dots = FALSE, border = 1, ppoly = FALSE, asp = 1, 
-#     xlab = "Longitude", ylab = "Latitude", main = "Study Area")
-# plotMaskEdge(mask_plot, add = TRUE, col = "black", lwd = 1.5) 
-# points(trap, pch = 4, col = "black", cex = 1.5)
-# title(main = "Study Area with Trap Locations", col.main = "black", font.main = 2, cex.main = 1.5)
-
+ # mask_plot = make.mask(trap, buffer = 2, spacing = 0.01, type = "trapbuffer")
+ # # Get plot limits from the mask
+ # xlim <- range(mask_plot$x)
+ # ylim <- range(mask_plot$y)
+ # plot(mask_plot, dots = FALSE, border = 1, ppoly = FALSE, asp = 1, 
+ #      xlab = "Longitude", ylab = "Latitude", main = "Study Area",
+ #      xlim = xlim, ylim = ylim)
+ # plotMaskEdge(mask_plot, add = TRUE, col = "black", lwd = 1.5) 
+ # points(trap, pch = 4, col = "black", cex = 1.5)
+ # axis(1, at = seq(floor(xlim[1]/5)*5, ceiling(xlim[2]/5)*5, by = 5),
+ #      col = "black", col.axis = "black", cex.axis = 1.2)
+ # 
+ # axis(2, at = seq(floor(ylim[1]/5)*5, ceiling(ylim[2]/5)*5, by = 5),
+ #      col = "black", col.axis = "black", cex.axis = 1.2)
+ # mtext("Easting", side = 1, line = 3, cex = 1.1)
+ # mtext("Northing", side = 2, line = 3, cex = 1.1)
+ # legend("topright", legend = "Traps", pch = 4, col = "black", pt.cex = 1.5,
+ #        cex = 1, bty = "n", text.col = "black")
+ # title(main = "Study Area with Trap Locations", col.main = "black", font.main = 2, cex.main = 1.5)
+ # 
 
 ## set the length of the survey as T=12 days, the time 
 ## discretization with L=100 time intervals, the 
 ## number of observed individuals n 
-T<-12
+T<-11
 L<-100
 n<-length(unique(data$id))
 
@@ -84,6 +97,10 @@ time_nomem<- end_time - start_time
 param_nm<-confint_param(fit_nomem,T,traps,mask)
 N_nm<-confint_pop(fit_nomem,T,traps,mask,n,distribution = "binomial", loginterval = TRUE, alpha = 0.05)
 
+
 ## calculate the difference in AIC
 theta_est_nomem<-fit_nomem$par
 (2*fit_nomem$value-2*2)-(2*fit$value - 2*3)
+
+
+
